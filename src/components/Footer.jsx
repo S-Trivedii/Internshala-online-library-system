40104a90762ecc80292e7c43ce76cd54f6647677
Shubhanshu Trivedi <itsshubhanshu03@gmail.com>
2025-04-16 20:02:54 +0530

    update link for github

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
       <div className="max-w-[1200px] mx-auto px-4 text-center">
         <p className="mb-2">Made with ❤️ by Shubhanshu</p>
         <Link
-          to="https://www.github.com"
+          to="https://github.com/S-Trivedii/Internshala-online-library-system"
           target="_blank"
           rel="noopener noreferrer"
           className="text-blue-400 hover:underline"
